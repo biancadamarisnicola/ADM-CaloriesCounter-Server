c65fa3970f38731aa3e0795e41bcec0a56791266
Bia <damaris.nicola@gmail.com>
2016-11-30 20:58:37 +0200

    Add authentication to server

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const Koa = require('koa')
     , server = require('http').createServer(app.callback())
     , io = require('socket.io')(server)
     , cors = require('koa-cors')
+    , jwt = require('koa-jwt')
     , convert = require('koa-convert')
     , bodyparser = require('koa-bodyparser')
     , router = require('koa-router')()
     , datastore = require('nedb-promise')
-    , alimentRepo = datastore({filename: './aliments.json', autoload: true});
+    , alimentRepo = datastore({filename: './aliments.json', autoload: true})
+    , userRepo = datastore({filename: './users.json', autoload: true});
 
 let alimentsLastUpdate = null;
 
@@ -30,6 +32,8 @@ app.use(bodyparser());
 app.use(convert(cors()));
 
 const ALIMENT = '/Aliment'
+    , LOGIN = '/api/auth/session'
+    , SIGNUP = '/api/signup'
     , LAST_MODIFIED = 'Last-Modified'
     , ETAG = 'ETag'
     , OK = 200
@@ -40,6 +44,21 @@ const ALIMENT = '/Aliment'
     , NOT_FOUND = 404
     , METHOD_NOT_ALLOWED = 405
     , CONFLICT = 409;
+
+const jwtConfig = {
+    secret: 'bias-secret'
+}
+
+function createToken(user) {
+    console.log(`create token for ${user.username}`);
+    return jwt.sign({username: user.username, _id: user._id}, jwtConfig.secret, { expiresIn: 60*60*60 });
+}
+
+function decodeToken(token) {
+    let decoded = jwt.decode(token, jwtConfig.secret);
+    console.log(`decoded token for ${decoded}`);
+    return decoded;
+}
 
 router
     .get(ALIMENT, async(ctx) => {
@@ -74,6 +93,44 @@ router
             setIssueRes(res, BAD_REQUEST, [{error: 'Some field is missing'}]); //400 Bad Request
         }
     })
+    .post([SIGNUP], async(ctx)=> {
+        let user = ctx.request.body;
+        let res = ctx.response;
+        if (!user.username || !user.password) {
+            console.log(`session - missing username and password`);
+            setIssueRes(ctx.response, BAD_REQUEST, [{error: 'Both username and password must be set'}])
+            return;
+        }
+        await createUser(res, user);
+        if (user && user.password) {
+            ctx.status = CREATED;
+            let tkn = createToken(user);
+            ctx.response.body = {token: tkn};
+            console.log(`session - token created `+tkn);
+        } else {
+            console.log(`session - wrong password`);
+            setIssueRes(ctx.response, BAD_REQUEST, [{error: 'Wrong password'}])
+        }
+    })
+    .post([LOGIN], async(ctx) => {
+        let reqBody = ctx.request.body;
+        console.log(reqBody);
+        if (!reqBody.username || !reqBody.password) {
+            console.log(`session - missing username and password`);
+            setIssueRes(ctx.response, BAD_REQUEST, [{error: 'Both username and password must be set'}])
+            return;
+        }
+        console.log(reqBody.username);
+        let user = await userRepo.findOne({username: reqBody.username})
+        if (user && user.password === reqBody.password) {
+            ctx.status = CREATED;
+            ctx.response.body = {token: createToken(user)};
+            console.log(`session - token created`);
+        } else {
+            console.log(`session - wrong password`);
+            setIssueRes(ctx.response, BAD_REQUEST, [{error: 'Wrong password'}])
+        }
+    })
     .put([ALIMENT, ':name'].join('/'), async(ctx) => {
         let aliment = ctx.request.body;
         let name = ctx.params.name;
@@ -96,7 +153,7 @@ router
                 let updatedCount = await alimentRepo.update({name: name}, aliment);
                 setAlimentRes(res, OK, aliment); //200 Ok
                 io.emit('aliment-updated', aliment);
-            }else {
+            } else {
                 setIssueRes(res, METHOD_NOT_ALLOWED, [{error: 'Aliment no longer exists'}]); //Method Not Allowed
             }
         }
@@ -123,9 +180,21 @@ const createAliment = async(res, aliment) => {
     io.emit('aliment-created', insertedAliment);
 }
 
+const createUser = async(res, user) => {
+    let insertedUser = await userRepo.insert(user);
+    setUserRes(res, CREATED, insertedUser); //201 Created
+    io.emit('user-created', insertedUser);
+}
+
 const setAlimentRes = (res, status, aliment) => {
     res.body = aliment;
     res.set({[ETAG]: aliment.version, [LAST_MODIFIED]: new Date(aliment.updated)});
+    res.status = status; //200 Ok or 201 Created
+}
+
+const setUserRes = (res, status, user) => {
+    res.body = user;
+    res.set({[ETAG]: user.username});
     res.status = status; //200 Ok or 201 Created
 }
 
@@ -143,7 +212,7 @@ io.on('connection', (socket) => {
 (async() => {
     await alimentRepo.remove({});
     for (let i = 0; i < 20; i++) {
-        await alimentRepo.insert({name: `Aliment ${i}`, calories: 100, proteins: 12.0, carbs: 80.3, fats: 6.0});
+        //await alimentRepo.insert({name: `Aliment ${i}`, calories: 100, proteins: 12.0, carbs: 80.3, fats: 6.0});
         console.log(`Aliment ${i} added`);
     }
 })();
